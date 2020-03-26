ff96fed557bb3272a586b8131f08a7713a874473
FIFA-legend <kolodkonikitos@gmail.com>
2020-03-26 19:35:34 +0300

    ``commit genius``
    (cherry picked from commit 9001e0a8786375f958ebdd835e7b50a0d470702d)
    
    # Conflicts:
    #	untitled/src/main.java

--- untitled/src/main.java
+++ untitled/src/main.java
@@ -2,13 +2,5 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola");
-        double i = 17.4;
-        System.out.println("дарова");
-        i = 3.14;
-        System.out.println("sdgadgasgasg");
-        System.out.println("commit 1");
-        System.out.println("commit 2");
-        System.out.println("commit 3");
-        System.out.println("commit 4");
     }
 }
